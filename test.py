a6bef4ba15c354298f77c9be64e835eca8bd053c
Benjamin Nussbaum <bn9@illinois.edu>
2025-01-07 11:28:04 -0600

    Add uv build

--- test.py
+++ test.py
@@ -1,1 +1,36 @@
-print("Hello World!")
+from collections import deque
+import sys
+import time
+
+from pyfirmata2 import Arduino
+
+from pqnstack.pqn.drivers.polarimeter import ArduinoPolarimeter
+
+if __name__ == "__main__":
+    board: Arduino = Arduino(Arduino.AUTODETECT)
+    board.samplingOn(1000 // 4)
+    buffer = deque(maxlen=10)
+    print(board.analog)
+    board.analog[0].register_callback(buffer.append)
+    board.analog[0].enable_reporting()
+    # polarimeter = ArduinoPolarimeter(board=board)
+    # polarimeter.start_normalizing()
+
+    try:
+        while True:
+            # print(buffer)
+            # print(polarimeter._buffers[0])
+            # print(polarimeter)
+            # input()
+            # result = polarimeter.read()
+            # print(f"{result:.2f} {result.theta=:+.2f}")
+            # input()
+            sys.stdout.write(f"\r{buffer}")
+            sys.stdout.flush()
+            time.sleep(0.05)
+
+    except KeyboardInterrupt:
+        sys.stdout.write("\n")
+    finally:
+        board.exit()
+        # polarimeter.close()

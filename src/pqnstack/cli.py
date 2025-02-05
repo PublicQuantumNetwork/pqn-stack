535309116d23f59fa74cdfebdb2bef7a6bd4d2a4
marcosf2 <marcosf2@illinois.edu>
2025-02-05 12:44:00 -0600

    Convert CLI into typer CLI

--- src/pqnstack/cli.py
+++ src/pqnstack/cli.py
@@ -1,8 +1,10 @@
-import argparse
 import json
 import logging
 import tomllib
 from pathlib import Path
+from typing import Annotated
+
+import typer
 
 from pqnstack.base.errors import InvalidNetworkConfigurationError
 from pqnstack.network.node import Node
@@ -12,6 +14,8 @@ from pqnstack.network.router import Router
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+app = typer.Typer(no_args_is_help=True, help="CLI for PQN-Stack.")
 
 
 def _verify_instruments_config(instruments: list[dict[str, str]]) -> dict[str, dict[str, str]]:
@@ -65,73 +69,59 @@ def _load_and_parse_node_config(
     return kwargs, instruments
 
 
-def start_node() -> None:
-    parser = argparse.ArgumentParser(
-        description="Start a node",
-        epilog="Starts a PQN Node. Can be configured by passing arguments directly into the command line but it is recommended to use a config file if instruments will added.",
-    )
-    parser.add_argument("-n", "--name", type=str, required=False, help="Name of the node")
-    parser.add_argument(
-        "-rn",
-        "--router_name",
-        type=str,
-        required=False,
-        help="Name of the router this node will talk to (default: 'router1').",
-    )
-    parser.add_argument(
-        "-ho",
-        "--host",
-        type=str,
-        required=False,
-        help="Host address (IP) of the node (default: 'localhost'). Usually the IP address of the Router this node will talk to.",
-    )
-    parser.add_argument(
-        "-p",
-        "--port",
-        type=int,
-        required=False,
-        help="Port of the node (default: 5555). Has to be the same port as the Router.",
-    )
-    parser.add_argument(
-        "-i",
-        "--instruments",
-        type=str,
-        required=False,
-        help='JSON formatted string with necessary arguments to instantiate instruments. Example: \'{"dummy1": {"import": "pqnstack.pqn.drivers.dummies.DummyInstrument", "desc": "Dummy Instrument 1", "address": "123456"}}\'',
-    )
-    parser.add_argument(
-        "-c",
-        "--config",
-        type=str,
-        required=False,
-        help="Path to the config file, will get overridden by command line arguments.",
-    )
+@app.command()
+def start_node(  # noqa: PLR0913
+    name: Annotated[str | None, typer.Option(help="Name of the Node.")] = None,
+    router_name: Annotated[
+        str | None, typer.Option(help="Name of the router this node will talk to (default: 'router1').")
+    ] = None,
+    host: Annotated[
+        str | None,
+        typer.Option(
+            help="Host address (IP) of the node (default: 'localhost'). Usually the IP address of the Router this node will talk to."
+        ),
+    ] = None,
+    port: Annotated[
+        int | None, typer.Option(help="Port of the node (default: 5555). Has to be the same port as the Router.")
+    ] = None,
+    instruments: Annotated[
+        str | None,
+        typer.Option(
+            help='JSON formatted string with necessary arguments to instantiate instruments. Example: \'{"dummy1": {"import": "pqnstack.pqn.drivers.dummies.DummyInstrument", "desc": "Dummy Instrument 1", "address": "123456"}}\''
+        ),
+    ] = None,
+    config_path: Annotated[
+        str | None, typer.Option(help="Path to the config file, will get overridden by command line arguments.")
+    ] = None,
+) -> None:
+    """
+    Start a PQN Node.
 
-    args = parser.parse_args()
-
+    Can be configured by passing arguments directly into the command line but it is recommended to use a config file if instruments will be added.
+    """
     kwargs: dict[str, str | int] = {}
-    instruments: dict[str, dict[str, str]] = {}
+    ins: dict[str, dict[str, str]] = {}
 
-    if args.config:
-        kwargs, instruments = _load_and_parse_node_config(args.config, kwargs, instruments)
+    if config_path:
+        kwargs, ins = _load_and_parse_node_config(config_path, kwargs, ins)
 
-    if args.name:
-        kwargs["name"] = args.name
-    if args.router_name:
-        kwargs["router_name"] = args.router_name
-    if args.host:
-        kwargs["host"] = args.host
-    if args.port:
-        kwargs["port"] = args.port
-    if args.instruments:
+    if name:
+        kwargs["name"] = name
+    if router_name:
+        kwargs["router_name"] = router_name
+    if host:
+        kwargs["host"] = host
+    if port:
+        kwargs["port"] = port
+    if instruments:
         # We don't want to override instruments, instead combining them with the ones from config file is cleaner behaviour.
-        instruments = {**instruments, **json.loads(args.instruments)}
+        ins = {**ins, **json.loads(instruments)}
 
     if "name" not in kwargs:
         msg = "Node name is required"
         raise InvalidNetworkConfigurationError(msg)
 
-    node = Node(**kwargs, **instruments)  # type: ignore[arg-type]
+    node = Node(**kwargs, **ins)  # type: ignore[arg-type]
     node.start()
 
 
@@ -152,40 +142,35 @@ def _load_and_parse_router_config(config_path: Path | str, kwargs: dict[str, str
     return kwargs
 
 
-def start_router() -> None:
-    parser = argparse.ArgumentParser(
-        description="Start a router",
-        epilog="Starts a PQN Router. Can be configured by passing arguments directly into the command line or through a config file. ",
-    )
+@app.command()
+def start_router(
+    name: Annotated[str | None, typer.Option(help="Name of the router (default 'router1')")] = None,
+    host: Annotated[
+        str | None,
+        typer.Option(
+            help="Host address (IP) of the router (default: 'localhost'). Usually the IP address of the machine running the router."
+        ),
+    ] = None,
+    port: Annotated[str | None, typer.Option(help="Port of the router (default: 5555)")] = None,
+    config: Annotated[
+        str | None, typer.Option(help="Path to the config file, will get overridden by command line arguments.")
+    ] = None,
+) -> None:
+    """
+    Start a PQN Router.
 
-    parser.add_argument("-n", "--name", type=str, required=False, help="Name of the router")
-    parser.add_argument(
-        "-ho",
-        "--host",
-        type=str,
-        required=False,
-        help="Host address (IP) of the router (default: 'localhost'). Usually the IP address of the machine running the router.",
-    )
-    parser.add_argument("-p", "--port", type=int, required=False, help="Port of the router (default: 5555)")
-    parser.add_argument(
-        "-c",
-        "--config",
-        type=str,
-        required=False,
-        help="Path to the config file, will get overridden by command line arguments.",
-    )
-
-    args = parser.parse_args()
+    Can be configured by passing arguments directly into the command line or through a config file.
+    """
     kwargs: dict[str, str | int] = {}
-    if args.config:
-        kwargs = _load_and_parse_router_config(args.config, kwargs)
+    if config:
+        kwargs = _load_and_parse_router_config(config, kwargs)
 
-    if args.name:
-        kwargs["name"] = args.name
-    if args.host:
-        kwargs["host"] = args.host
-    if args.port:
-        kwargs["port"] = int(args.port)
+    if name:
+        kwargs["name"] = name
+    if host:
+        kwargs["host"] = host
+    if port:
+        kwargs["port"] = int(port)
 
     if "name" not in kwargs:
         msg = "Router name is required"
@@ -194,3 +179,7 @@ def start_router() -> None:
     # mypy doesn't like **kwargs https://github.com/python/mypy/issues/5382#issuecomment-417433738
     router = Router(**kwargs)  # type: ignore[arg-type]
     router.start()
+
+
+if __name__ == "__main__":
+    app()

8a0e719bd078695d3ed1bc6b1504122447e5b2f3
Benjamin Nussbaum <bn9@illinois.edu>
2025-04-22 18:56:13 -0500

    First pass porting timetagger (more to do...)

--- src/pqnstack/pqn/drivers/timetagger.py
+++ src/pqnstack/pqn/drivers/timetagger.py
@@ -1,44 +1,26 @@
-from __future__ import annotations
-
 import logging
-from abc import abstractmethod
 from dataclasses import dataclass
 from dataclasses import field
-from typing import TYPE_CHECKING
 from typing import Any
+from typing import Protocol
+from typing import runtime_checkable
 
-import numpy as np
-import TimeTagger
 from TimeTagger import ChannelEdge
 from TimeTagger import Correlation
 from TimeTagger import Counter
+from TimeTagger import TimeTagger
 from TimeTagger import createTimeTaggerNetwork
 from TimeTagger import freeTimeTagger
 
-from pqnstack.base.driver import DeviceClass
-from pqnstack.base.driver import DeviceDriver
-from pqnstack.base.driver import DeviceInfo
-from pqnstack.base.driver import DeviceStatus
-
-if TYPE_CHECKING:
-    from collections.abc import Callable
+from pqnstack.base.instrument import Instrument
+from pqnstack.base.instrument import InstrumentInfo
+from pqnstack.base.instrument import log_parameter
 
 logger = logging.getLogger(__name__)
 
 
-@dataclass
-class MeasurementConfig:
-    duration: int  # in picoseconds
-    binwidth: int = 500  # in picoseconds
-    channel1: int = 1
-    channel2: int = 2
-    dark_count: int = 0
-
-
-@dataclass
-class TimeTaggerInfo(DeviceInfo):
-    """Metadata and current state information for the time tagger device."""
-
+@dataclass(frozen=True, slots=True)
+class TimeTaggerInfo(InstrumentInfo):
     channels_in_use: list[int] = field(default_factory=list)
     test_signal_enabled: bool = False
     test_signal_divider: int = 1
@@ -46,35 +28,45 @@ class TimeTaggerInfo(DeviceInfo):
     last_coincidence_window_ps: int = 0
 
 
-class TimeTaggerDevice(DeviceDriver):
-    """
-    Abstract base class for a time tagger device.
+@runtime_checkable
+@dataclass(slots=True)
+class TimeTaggerInstrument(Instrument, Protocol):
+    channels_to_use: int = 10
+    channels_in_use: list[int] = field(init=False, default_factory=list)
 
-    Inherit from this class to implement specialized time tagger drivers, such as one for Swabian Instruments hardware.
-    """
+    def __post_init__(self) -> None:
+        self.operations["measure_coincidence"] = self.measure_coincidence
+        self.operations["measure_countrate"] = self.measure_countrate
 
-    DEVICE_CLASS = DeviceClass.TIMETAGR
+        self.parameters.add("degrees")
 
-    @abstractmethod
+    @property
     def info(self) -> TimeTaggerInfo:
-        """Return a dataclass containing metadata about the time tagger."""
+        return TimeTaggerInfo(
+            name=self.name,
+            desc=self.desc,
+            hw_address=self.hw_address,
+            hw_status=self.hw_status,
+        )
 
-    @abstractmethod
-    def close(self) -> None:
-        """Shut down and release resources for the time tagger."""
+    @property
+    @log_parameter
+    def degrees(self) -> float: ...
 
-    @abstractmethod
-    def start(self) -> None:
-        """
-        Initialize and start the time tagger hardware.
+    @degrees.setter
+    @log_parameter
+    def degrees(self, degrees: float) -> None: ...
 
-        May optionally set up channel usage, test signal parameters, etc.
-        """
-        #    @abstractmethod
-        #    def measure_coincidence(
-        #        self, groups: list[tuple[int, ...]], measurement_time: float = 5.0, coincidence_window_ps: int = 10000
-        #    ) -> None:
-        """Perform a coincidence-counting measurement on a given list of channel groups, for the specified real-time measurement duration and coincidence window."""
+    def measure_coincidence(self) -> float: ...
+    def measure_countrate(self) -> float: ...
+
+    def move_to(self, angle: float) -> None:
+        """Move the rotator to the specified angle."""
+        self.degrees = angle
+
+    def move_by(self, angle: float) -> None:
+        """Move the rotator by the specified angle."""
+        self.degrees += angle
 
     @abstractmethod
     def measure_coincidence(self, channel1: int, channel2: int, binwidth_ps: int, measurement_duration_ps: int) -> int:
@@ -85,33 +77,11 @@ class TimeTaggerDevice(DeviceDriver):
         "Measaures the singles counts on input channels."
 
 
-class SwabianTimeTagger(TimeTaggerDevice):
-    def __init__(
-        self,
-        name: str,
-        desc: str,
-        address: str,
-        channels_to_use: int = 10,
-    ) -> None:
-        """
-        Control the Swabian time tagger using a wrapper class.
-
-        :param name: Name of the device (must not contain ':').
-        :param desc: Descriptive string for the device.
-        :param address: Unique identifier (e.g., device serial number).
-        """
-        super().__init__(name, desc, address)
-
-        self._tagger: TimeTagger = None
-        self._channels_in_use: list[int] = []
-        self.channels_to_use = channels_to_use
-        self._test_signal_enabled: bool = False
-        self._test_signal_divider: int = 1
-        self.operations: dict[str, Callable[..., Any]] = {
-            "measure_coincidence": self.measure_coincidence,
-            "measure_countrate": self.measure_countrate,
-        }
-        self.status = DeviceStatus.OFF
+@dataclass(slots=True)
+class SwabianTimeTagger(TimeTaggerInstrument):
+    _tagger: TimeTagger = field(init=False, repr=False)
+    _test_signal_enabled: bool = False
+    _test_signal_divider: int = 1
 
     def start(self) -> None:
         """Initialize the connection to the Swabian time tagger hardware and configures channels for potential coincidence counting."""
@@ -127,14 +97,13 @@ class SwabianTimeTagger(TimeTaggerDevice):
             raise RuntimeError(msg)
 
         all_channels = self._tagger.getChannelList(ChannelEdge.Rising)
-        self._channels_in_use = all_channels[: self.channels_to_use]
+        self.channels_in_use = all_channels[: self.channels_to_use]
 
-        logger.info("Channels in use: %s", self._channels_in_use)
+        logger.info("Channels in use: %s", self.channels_in_use)
 
-        for ch in self._channels_in_use:
+        for ch in self.channels_in_use:
             self._tagger.setInputDelay(ch, 0)
 
-        self.status = DeviceStatus.READY
         logger.info("Swabian Time Tagger device is now READY.")
 
     def close(self) -> None:
@@ -144,21 +113,7 @@ class SwabianTimeTagger(TimeTaggerDevice):
             freeTimeTagger(self._tagger)
             self._tagger = None
 
-        self.status = DeviceStatus.OFF
         logger.info("Swabian Time Tagger device is now OFF.")
-
-    def info(self) -> TimeTaggerInfo:
-        """Return information about this device, including its current parameters and measurement settings."""
-        return TimeTaggerInfo(
-            name=self.name,
-            desc=self.desc,
-            address=self.address,
-            dtype=self.DEVICE_CLASS,
-            status=self.status,
-            channels_in_use=self._channels_in_use,
-            test_signal_enabled=self._test_signal_enabled,
-            test_signal_divider=self._test_signal_divider,
-        )
 
     def set_test_signal(self, channels: list[int], *, enable: bool = True, divider: int | None = None) -> None:
         self._tagger.setTestSignal(channels, enable)
@@ -168,17 +123,27 @@ class SwabianTimeTagger(TimeTaggerDevice):
     def set_input_delay(self, channel: int, delay_ps: int) -> None:
         self._tagger.setInputDelay(channel, delay_ps)
 
-    def measure_countrate(self, channels: list[int], binwidth_ps: int) -> list[int]:
-        counter = Counter(self._tagger, channels, binwidth_ps, 1)
-        counter.startFor(binwidth_ps)
+    def measure_singles(self, channels: list[int], count_time_s: float) -> list[int]:
+        # TODO: use these as kwargs
+        count_time_ps = int(count_time_s * 1e12)
+        counter = Counter(self._tagger, channels, count_time_ps, 1)
+        counter.startFor(count_time_ps)
         counter.waitUntilFinished()
         return [item[0] for item in counter.getData()]
 
-    def measure_coincidence(self, channel1: int, channel2: int, binwidth_ps: int, measurement_duration_ps: int) -> int:
+    def measure_coincidence(self, channel1: int, channel2: int, count_time_s: float, binwidth_s: float) -> int:
+        # TODO: use these as kwargs
+        count_time_ps = int(count_time_s * 1e12)
+        binwidth_ps = int(binwidth_s * 1e12)
         corr = Correlation(self._tagger, channel1, channel2, binwidth_ps, n_bins=100000)
-        corr.startFor(measurement_duration_ps)
+        corr.startFor(count_time_ps)
         corr.waitUntilFinished()
-        return int(np.max(corr.getData()))
+        counts = max(corr.getData())
+
+        if not isinstance(counts, int):
+            return -1
+
+        return counts
 
     def enable_test_signal(self, *, enabled: bool, test_signal_divider: int = 1) -> None:
         self.enabled = enabled
@@ -186,8 +151,8 @@ class SwabianTimeTagger(TimeTaggerDevice):
         self._test_signal_enabled = self.enabled
         self.test_signal_divider = self.test_signal_divider
         if self._test_signal_enabled:
-            self._tagger.setTestSignal(self._channels_in_use, enable=True)
-            logger.info("Test signal enabled on channels: %s", self._channels_in_use)
+            self._tagger.setTestSignal(self.channels_in_use, enable=True)
+            logger.info("Test signal enabled on channels: %s", self.channels_in_use)
             if self.test_signal_divider != 1:
                 self._tagger.setTestSignalDivider(self.test_signal_divider)
                 logger.info("Test signal divider set to %d", self.test_signal_divider)

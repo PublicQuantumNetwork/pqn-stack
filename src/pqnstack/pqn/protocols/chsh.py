7df96b85a2b868e954d1ddf279b15464741499f4
Soroush Hoseini <soroushhoseini0@gmail.com>
2025-05-13 21:16:42 -0500

    Reformat CHSH to move low level wp calculation to expectation value

--- src/pqnstack/pqn/protocols/chsh.py
+++ src/pqnstack/pqn/protocols/chsh.py
@@ -48,58 +48,66 @@ def calculate_chsh_error(error_values: list[float]) -> float:
 
 
 def basis_to_wp(basis: float) -> list[float]:
-    return [basis / 2, 0.0]
+    return [basis / 2, 0.0]  # TODO: Make input a complex number and have the quarter waveplate angle calculated from it
 
 
 def expectation_value(
-    devices: Devices, config: MeasurementConfig, angles: Angles
+    devices: Devices, config: MeasurementConfig, base1: float, base2: float
 ) -> tuple[float, float, dict[str, object]]:
-    angles1 = [[angles.idler_hwp, angles.idler_qwp], [angles.idler_hwp + 45, angles.idler_qwp]]
-    angles2 = [[angles.signal_hwp, angles.signal_qwp], [angles.signal_hwp + 45, angles.signal_qwp]]
+    idler_wp_angles = basis_to_wp(base1)
+    signal_wp_angles = basis_to_wp(base2)
+
+    angles_idler = [idler_wp_angles, [idler_wp_angles[0] + 45, idler_wp_angles[1]]]
+    angles_signal = [signal_wp_angles, [signal_wp_angles[0] + 45, signal_wp_angles[1]]]
+
     coincidence_counts = []
-    for angle1 in angles1:
-        for angle2 in angles2:
-            devices.idler_hwp.move_to(angle1[0])
-            devices.idler_qwp.move_to(angle1[1])
-            devices.signal_hwp.move_to(angle2[0])
-            devices.signal_qwp.move_to(angle2[1])
+    for angle_idler in angles_idler:
+        for angle_signal in angles_signal:
+            devices.idler_hwp.move_to(angle_idler[0])
+            devices.idler_qwp.move_to(angle_idler[1])
+            devices.signal_hwp.move_to(angle_signal[0])
+            devices.signal_qwp.move_to(angle_signal[1])
             sleep(2)
             counts = devices.timetagger.measure_coincidence(
                 config.channel1, config.channel2, int(config.binwidth * 1e12), int(config.duration * 1e12)
             )
             coincidence_counts.append(counts)
+
     numerator = coincidence_counts[0] - coincidence_counts[1] - coincidence_counts[2] + coincidence_counts[3]
     denominator = sum(coincidence_counts) - 4 * config.dark_count
     expectation_val = numerator / denominator
     expectation_error = calculate_chsh_expectation_error(coincidence_counts, config.dark_count)
+
     raw_results = {
         "timestamp": datetime.datetime.now(datetime.UTC).isoformat(),
-        "input_angles1": angles1,
-        "input_angles2": angles2,
+        "input_base1": base1,
+        "input_base2": base2,
+        "idler_wp_angles": angles_idler,
+        "signal_wp_angles": angles_signal,
         "raw_counts": coincidence_counts,
         "raw_error": expectation_error,
     }
+
     return float(expectation_val), expectation_error, raw_results
 
 
 def measure_chsh(
     basis1: list[float], basis2: list[float], devices: Devices, config: MeasurementConfig
 ) -> dict[str, object]:
-    angles1 = [basis_to_wp(element) for element in basis1]
-    angles2 = [basis_to_wp(element) for element in basis2]
     expectation_values = []
     expectation_errors = []
     raw_results = []
-    for angle1 in angles1:
-        for angle2 in angles2:
-            exp_val, exp_err, raw = expectation_value(
-                devices, config, Angles(angle1[0], angle1[1], angle2[0], angle2[1])
-            )
+
+    for base1 in basis1:
+        for base2 in basis2:
+            exp_val, exp_err, raw = expectation_value(devices, config, base1, base2)
             expectation_values.append(exp_val)
             expectation_errors.append(exp_err)
             raw_results.append(raw)
+
     chsh_value = -1 * expectation_values[0] + expectation_values[1] + expectation_values[2] + expectation_values[3]
     chsh_error = calculate_chsh_error(expectation_errors)
+
     return {
         "timestamp": datetime.datetime.now(datetime.UTC).isoformat(),
         "raw_results": raw_results,

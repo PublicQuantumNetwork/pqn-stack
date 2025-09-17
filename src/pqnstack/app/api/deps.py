81315258c43b8b958e4c5450acfed29379042c79
marcosf2 <marcosf2@illinois.edu>
2025-09-17 11:34:33 -0500

    Removed CoordinationState object

--- src/pqnstack/app/api/deps.py
+++ src/pqnstack/app/api/deps.py
@@ -4,8 +4,8 @@ from typing import Annotated
 import httpx
 from fastapi import Depends
 
-from pqnstack.app.core.config import CoordinationState
-from pqnstack.app.core.config import state
+from pqnstack.app.core.config import NodeState
+from pqnstack.app.core.config import get_state
 
 
 async def get_http_client() -> AsyncGenerator[httpx.AsyncClient, None]:
@@ -16,8 +16,4 @@ async def get_http_client() -> AsyncGenerator[httpx.AsyncClient, None]:
 ClientDep = Annotated[httpx.AsyncClient, Depends(get_http_client)]
 
 
-async def get_coordination_state() -> AsyncGenerator[CoordinationState, None]:
-    yield state.coordination_state
-
-
-CoordinationStateDep = Annotated[CoordinationState, Depends(get_coordination_state)]
+StateDep = Annotated[NodeState, Depends(get_state)]

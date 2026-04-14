9016f1214d9fc6aa5e405292ec2425a5169ebe39
SoroushHoseini <soroushhoseini0@gmail.com>
2026-04-14 15:58:39 -0500

    switchtest.py

---
+++ src/switchtest.py
@@ -0,0 +1,9 @@
+import logging
+from pqnstack.pqn.drivers.switch import Switch
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+logger = logging.getLogger(__name__)
+sw = Switch("192.168.0.1", 8008, "admin", "root")
+sw.add_patch(1, 9)
+#sw.remove_patch(1)
+

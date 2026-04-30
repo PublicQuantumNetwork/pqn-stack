60b9bd507889b931617c6c2242b4469655e2d904
Marcos Frenkel <marcosf2@illinois.edu>
2026-04-30 13:40:51 -0500

    Implement run_one fully

--- src/run_tomography.py
+++ src/run_tomography.py
@@ -4,6 +4,7 @@ import time
 import logging
 from pqnstack.network.client import Client
 from pqnstack.pqn.drivers.switch import Switch
+from pqnstack.constants import DEFAULT_SETTINGS
 from pqnstack.pqn.drivers.powermeter import PM100D
 from tomography import Devices, measure_tomography_raw
 from pqnstack.pqn.drivers.rotator import SerialRotator
@@ -12,36 +13,81 @@ from pqnstack.pqn.drivers.thorlabs_polarimeter import PAX1000IR2
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
-# Output filename
-# TODO: need to specify which run we are in somewhere
-log_filename = f"tomography_{int(time.time())}.csv"
-
-hwp = SerialRotator(name='hwp', desc='polarization meas setup', hw_address="/dev/ttyACM0")
-qwp = SerialRotator(name='qwp', desc='polarization meas setup', hw_address="/dev/ttyUSB0")
-hwp.start()
-qwp.start()
-
-# TODO: what to set for board and pins?
-polarimeter = PAX1000IR2(name="polarimeter", desc="polarimeter", hw_address="/dev/usbtmc0")
+# Connect to polarimeter
+logger.info("Connecting to polarimeter")
+polarimeter = PAX1000IR2(name="polarimeter", desc="polarimeter", hw_address="")
 polarimeter.start()
 
+# Connect to switch
+logger.info("Connecting to switch")
 switch = Switch("switch", "POLATIS test unit", "192.168.0.1", 8008, "admin", "root")
 switch.start()
 
-def log_data(data):
+# Connect to HWP
+logger.info("Connecting to HWP")
+hwp = SerialRotator(name='hwp', desc='polarization meas setup', hw_address="/dev/ttyACM0")
+hwp.start()
+
+# Connect to QWP
+logger.info("Connecting to QWP")
+qwp = SerialRotator(name='qwp', desc='polarization meas setup', hw_address="/dev/ttyUSB0")
+qwp.start()
+
+# Appends data to the CSV data file
+def record_data(data):
     global log_filename
     
     # TODO: change this to match new data format
-    headers = ["timestamp", "pm1_w", "pm1_ref_w", "pm1_total_w", "pax_wavelength_nm"]
-    
+    headers = ["timestamp", "pax_azimuth_deg", "pax_ellipticity_deg", "pax_theta_deg", "pax_eta_deg", "pax_dop", "pax_power_w", "pax_wavelength_nm"]
+
+    # Check if file exists
+    file_exists = os.path.exists(log_filename)
+
     with open(log_filename, mode="a", newline="", buffering=1) as file:
         writer = csv.DictWriter(file, fieldnames=headers)
 
         # Write header if new file
-        if file.tell() == 0:
+        if not file_exists:
             writer.writeheader()
 
+        # Append row
         writer.writerow(data)
+
+"""
+RUN 1
+Measure stability of H input over a 10min period
+Repeat for V, D, L, A, R
+"""
+def run_one(input_port, output_port):
+    global switch, polarimeter, hwp, qwp, log_filename
+
+    # Remove all existing patches
+    logger.info("Removing existing patches")
+    for i in range(8):
+        switch.remove_patch(i+1)
+
+    # Add a new patch from input_port to output_port
+    logger.info("Creating new patch")
+    switch.add_patch(input_port, output_port)
+
+    for state in DEFAULT_SETTINGS:
+        # Output filename
+        log_filename = f"tomography_{int(time.time())}_R1_{state}.csv"
+
+        # Move HWP and QWP to desired angles
+        logger.info(f"Moving rotators to {state} polarization")
+        hwp.move_to(DEFAULT_SETTINGS[state][0])
+        qwp.move_to(DEFAULT_SETTINGS[state][1])
+        time.sleep(1)
+
+        for i in range(60):
+            data = polarimeter.read()
+            data["timestamp"] = str(time.time())
+            print(data)
+            record_data(data)
+            time.sleep(10)
+
+        logger.info(f"Done recording data from {state} polarization")
 
 """
 RUN 2
@@ -49,7 +95,9 @@ Gather tomography data over 1 hour
 EO converter goes to switch input port, switch output goes to meter
 """
 def run_two(input_port, output_port):
-    global switch, polarimeter, signal_hwp, signal_qwp
+    global switch, polarimeter, hwp, qwp, log_filename
+
+    log_filename += "_R2.csv"
 
     # Remove all patches
     for i in range(8):
@@ -62,26 +110,28 @@ def run_two(input_port, output_port):
         signal_angles: tuple[float, float] = TOMOGRAPHY_BASIS.settings[signal_state]
         idler_angles: tuple[float, float] = TOMOGRAPHY_BASIS.settings[idler_state]
 
-        signal_hwp.move_to(signal_angles[0])
-        signal_qwp.move_to(signal_angles[1])
+        hwp.move_to(signal_angles[0])
+        qwp.move_to(signal_angles[1])
 
-        time.sleep(3)
+        time.sleep(1)
 
         data = meter.read()
         data["timestamp"] = str(time.time())
         print(polarimeter.read()) # TODO: figure out data format and add it into data{}
-        log_data(data)
+        record_data(data)
 
 """
 RUN 3
 Measures 1 sample, then rotates, repeat for 10 minutes
 """
 def run_three(input_port, output_port):
-    global switch, polarimeter, signal_hwp, signal_qwp
+    global switch, polarimeter, hwp, qwp, log_filename
+
+    log_filename += "_R3.csv"
 
     devices = Devices(
-        signal_hwp=signal_hwp,
-        signal_qwp=signal_qwp,
+        hwp=hwp,
+        qwp=qwp,
         timetagger=timetagger
     )
     config = MeasurementConfig(channel1=1, channel2=2, binwidth=1_000, duration=0.5)
@@ -95,6 +145,10 @@ RUN 4
 Measure crosstalk
 """
 def run_four():
+    global log_filename
+
+    log_filename += "_R4.csv"
+    
     pass # TODO
 
 """
@@ -102,14 +156,22 @@ RUN 5
 Measure extinction ratio
 """
 def run_five():
+    global log_filename
+
+    log_filename += "_R5.csv"
+
     pass # TODO
 
 try:
-    pass
-    #run_two(1, 9)
-    #run_three(1, 9)
+    run_one(1, 9)
 except KeyboardInterrupt:
-    pass
+    logger.warning("Received KeyboardInterrupt - halting")
 
 if (polarimeter):
     polarimeter.close()
+
+# Move HWP and QWP to desired angles
+logger.info("Resetting rotators")
+hwp.move_to(0)
+qwp.move_to(0)
+time.sleep(1)

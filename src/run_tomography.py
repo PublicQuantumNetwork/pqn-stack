0329de7c3c1ef4f39530479df55fc6ab270fbd02
Marcos Frenkel <marcosf2@illinois.edu>
2026-05-05 14:56:10 -0500

    Specify input/output port in filename

--- src/run_tomography.py
+++ src/run_tomography.py
@@ -15,6 +15,8 @@ from pqnstack.pqn.drivers.rotator import SerialRotator
 from pqnstack.pqn.drivers.thorlabs_polarimeter import PAX1000IR2
 
 OUTPUT_DIRECTORY = "../data"
+INPUT_PORT = 1
+OUTPUT_PORT = 9
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
@@ -42,7 +44,7 @@ qwp.start()
 # Appends data to the CSV data file
 def record_data(data, log_filename):
     # TODO: change this to match new data format
-    headers = ["timestamp", "pax_azimuth_deg", "pax_ellipticity_deg", "pax_theta_deg", "pax_eta_deg", "pax_dop", "pax_power_w", "pax_wavelength_nm"]
+    headers = ["timestamp", "polarization", "pax_azimuth_deg", "pax_ellipticity_deg", "pax_theta_deg", "pax_eta_deg", "pax_dop", "pax_power_w", "pax_wavelength_nm"]
 
     # Check if file exists
     file_exists = os.path.exists(log_filename)
@@ -58,7 +60,16 @@ def record_data(data, log_filename):
         writer.writerow(data)
 
 # Generates a data plot of the specified CSV file
-def plot_data(filename):
+def plot_data(
+    filename,
+    run,
+    state=None
+):
+    if (run == 1):
+        title = f"Tomography Data - Run 1, {state} Light"
+    elif (run == 2):
+        title = f"Tomography Data - Run 2, "
+
     # Set theme
     plt.style.use("seaborn-v0_8-white")
 
@@ -90,11 +101,7 @@ def plot_data(filename):
     ax2.set_ylabel("Azimuth (°)", color="green")
     ax2.tick_params(axis="y", labelcolor="green")
 
-    # Set title using filename
-    match = re.search(r"R(\d+)_([A-Z]+)", filename)
-    run_num = match.group(1)
-    light   = match.group(2)
-    plt.title(f"Tomography Data - Run {run_num}, {light} Light")
+    plt.title(title)
 
     # Save to PNG
     plt.savefig(filename.replace(".csv", ".png"), dpi=150, bbox_inches="tight")
@@ -111,21 +118,12 @@ RUN 1
 Measure stability of H input over a 10min period
 Repeat for V, D, L, A, R
 """
-def run_one(input_port, output_port):
+def run_one():
     global switch, polarimeter, hwp, qwp, OUTPUT_DIRECTORY
-
-    # Remove all existing patches
-    logger.info("Removing existing patches")
-    for i in range(8):
-        switch.remove_patch(i+1)
-
-    # Add a new patch from input_port to output_port
-    logger.info("Creating new patch")
-    switch.add_patch(input_port, output_port)
 
     for state in DEFAULT_SETTINGS:
         # Output filename
-        log_filename = f"{OUTPUT_DIRECTORY}/{get_timestamp()}_R1_{state}.csv"
+        log_filename = f"{OUTPUT_DIRECTORY}/{get_timestamp()}_R1_{state}_I{INPUT_PORT}-O{OUTPUT_PORT}.csv"
 
         # Move HWP and QWP to desired angles
         logger.info(f"Moving rotators to {state} polarization")
@@ -136,11 +134,12 @@ def run_one(input_port, output_port):
         for i in range(60):
             data = polarimeter.read()
             data["timestamp"] = str(time.time())
+            data["polarization"] = state
             record_data(data, log_filename)
             time.sleep(10)
 
         logger.info(f"Done recording data from {state} polarization")
-        plot_data(log_filename)
+        plot_data(log_filename, 1, state=state)
         logger.info("Generated data plot")
 
 """
@@ -149,29 +148,31 @@ Measure H, V, D, L, A, then R
 Wait 10 seconds
 Repeat whole process 40 times
 """
-def run_two(input_port, output_port):
-    global switch, polarimeter, hwp, qwp
+def run_two(duration):
+    global switch, polarimeter, hwp, qwp, OUTPUT_DIRECTORY
 
-    # Remove all patches
-    for i in range(8):
-        switch.remove_patch(i+1)
+    # Output filename
+    log_filename = f"{OUTPUT_DIRECTORY}/{get_timestamp()}_R2_I{INPUT_PORT}-O{OUTPUT_PORT}.csv"
 
-    switch.add_patch(inputport, output_port)
+    for i in range(int(duration*60/16)):
+        for state in DEFAULT_SETTINGS:
 
-    # Repeat once for each polarization state
-    for signal_state, idler_state in TOMOGRAPHY_BASIS.pairs:
-        signal_angles: tuple[float, float] = TOMOGRAPHY_BASIS.settings[signal_state]
-        idler_angles: tuple[float, float] = TOMOGRAPHY_BASIS.settings[idler_state]
+            # Move HWP and QWP to desired angles
+            logger.info(f"Moving rotators to {state} polarization")
+            hwp.move_to(DEFAULT_SETTINGS[state][0])
+            qwp.move_to(DEFAULT_SETTINGS[state][1])
+            time.sleep(1)
 
-        hwp.move_to(signal_angles[0])
-        qwp.move_to(signal_angles[1])
+            data = polarimeter.read()
+            data["timestamp"] = str(time.time())
+            data["polarization"] = state
+            record_data(data, log_filename)
 
-        time.sleep(1)
+        logger.info("Completed single tomography, wait 10s")
+        time.sleep(10)
 
-        data = meter.read()
-        #data["timestamp"] = str(time.time())
-        print(polarimeter.read()) # TODO: figure out data format and add it into data{}
-        record_data(data)
+    plot_data(log_filename, 2)
+    logger.info("Generated data plot")
 
 """
 RUN 3
@@ -190,23 +191,21 @@ def run_three(input_port, output_port):
     for i in range(200):
         # TODO: figure out data format and log it
         print(measure_tomography_raw(devices, config, 3))
-
-"""
-RUN 4
-Measure crosstalk
-"""
-def run_four():
-    pass # TODO
-
-"""
-RUN 5
-Measure extinction ratio
-"""
-def run_five():
     pass # TODO
 
 try:
-    run_one(1, 9)
+    # # Remove all existing patches
+    # logger.info("Removing existing patches")
+    # for i in range(8):
+    #     switch.remove_patch(i+1)
+
+    # # Add a new patch from INPUT_PORT to OUTPUT_PORT
+    # logger.info("Creating new patch")
+    # switch.add_patch(INPUT_PORT, OUTPUT_PORT)
+
+    # run_two(30)
+
+    plot_data("1777665237_2026-05-01_14-53-57_R2.csv")
 except KeyboardInterrupt:
     logger.warning("Received KeyboardInterrupt - halting")
 
